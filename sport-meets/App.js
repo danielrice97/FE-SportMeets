685cc96d158bb7995f4c1332d848c3cacefdb696
AGreaves99 <ajohngreaves@outlook.com>
2024-07-03 15:45:04 +0100

    removes unused imports

--- sport-meets/App.js
+++ sport-meets/App.js
@@ -1,6 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Tabs from "./navigation/Tabs";
 
 export default function App() {

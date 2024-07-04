855aa1e486b99b4fee727e5c09ac81b13e6b276e
AFF4NN <161034165+AFF4NN@users.noreply.github.com>
2024-07-04 14:42:05 +0100

    created event screen form, created account screen with login and register page links

--- sport-meets/App.js
+++ sport-meets/App.js
@@ -1,10 +1,20 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Tabs from "./navigation/Tabs";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stack.Navigator>
+        <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>3
     </NavigationContainer>
   );
 }
+

6a6c0f1e0dbae01c21f4bb6c643877f304107b54
AGreaves99 <ajohngreaves@outlook.com>
2024-07-05 14:12:20 +0100

    creates basic placeholder messages chat with hardcoded messages

--- sport-meets/screens/MessagesScreen.js
+++ sport-meets/screens/MessagesScreen.js
@@ -1,10 +1,48 @@
-import { View, Text } from "react-native";
+import { View, ScrollView, Text, FlatList } from "react-native";
 import React from "react";
 
-export default function MessagesScreen() {
+const testMessages = [
+  {
+    message_id: 1,
+    message_body: "Hi, I would like to join this event",
+    sender: "DannyBoy",
+    event_id: 1,
+  },
+  {
+    message_id: 2,
+    message_body: "Welcome to the world of Social Meets Up!",
+    sender: "Mo",
+    event_id: 1,
+  },
+  {
+    message_id: 3,
+    message_body: "Hey the weather is looking nice!",
+    sender: "Alex",
+    event_id: 1,
+  },
+];
+
+export default function MessagesScreen({ route, navigation }) {
+  const { name } = route.params;
+  const [messages, setMessages] = React.useState([])
+  React.useEffect(() => {
+    navigation.setOptions({
+      title: name,
+    });
+    setMessages(testMessages)
+  }, [])
+
   return (
-    <View>
-      <Text>MessagesScreen</Text>
-    </View>
+    <ScrollView>
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => item.message_id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.message_body}</Text>
+          </View>
+        )}
+      />
+    </ScrollView>
   );
 }

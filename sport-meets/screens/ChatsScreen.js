cd2e17bc09d39e58ec3385e5f85850da0b19a609
AGreaves99 <ajohngreaves@outlook.com>
2024-07-07 10:21:59 +0100

    updated ChatsScreen to import new GroupChatCard component

--- sport-meets/screens/ChatsScreen.js
+++ sport-meets/screens/ChatsScreen.js
@@ -1,72 +1,57 @@
 import {
   View,
-  Text,
   FlatList,
-  StyleSheet,
-  Pressable,
 } from "react-native";
-import React from "react";
+import {useState} from "react";
+import GroupChatCard from "../components/GroupChatCard";
 
 const testEventChats = [
   {
     event_id: 1,
-    event_name: "Football with the boys",
-    created_at: "13-07-2024 14:00:00",
-    event_organiser: "Mo",
+    event_name: "Soccer Star Solutions",
+    event_img_url:
+      "https://cdn.pixabay.com/photo/2016/05/27/14/33/football-1419954_640.jpg",
+    event_description: "Playing football with Northcoders Colleagues",
+    event_location: "Leeds",
+    created_at: "2024-07-19 10:30:00",
+    event_spaces_available: 20,
+    event_category: "football",
+    event_organiser: "Alex",
   },
   {
     event_id: 2,
-    event_name: "Golf practice at the range",
-    created_at: "12-08-2024 11:00:00",
-    event_organiser: "Mo",
+    event_name: "Bounce Ballers",
+    event_img_url: "https://storage.googleapis.com/pod_public/1300/180358.jpg",
+    event_description: "Playing Basketball 5 v 5",
+    event_location: "Leeds",
+    created_at: "2024-07-24 16:45:00",
+    event_spaces_available: 10,
+    event_category: "basketball",
+    event_organiser: "DannyBoy",
   },
   {
     event_id: 3,
-    event_name: "Rugby in the rain",
-    created_at: "21-09-2024 17:00:00",
+    event_name: "Birdie Bound",
+    event_img_url:
+      "https://cdn.shopify.com/s/files/1/0576/2750/8872/files/Golf_Birdie_480x480.jpg?v=1676301047",
+    event_description: "Playing Golf with Big Boys",
+    event_location: "Manchester",
+    created_at: "2024-07-24 11:00:00",
+    event_spaces_available: 5,
+    event_category: "Golf",
     event_organiser: "Mo",
   },
 ];
 
 export default function ChatsScreen({navigation}) {
+  const [joinedEvents, setJoinedEvents] = useState(testEventChats);
   return (
-    <View style={styles.container}>
+    <View>
       <FlatList
-        data={testEventChats}
+        data={joinedEvents}
         keyExtractor={(item) => item.event_id}
-        renderItem={({ item }) => (
-          <Pressable style={styles.event} onPress={() => navigation.navigate("Messages", {
-            name: item.event_name,
-            id: item.event_id
-          })}>
-            <Text style={styles.text}>{item.event_name}</Text>
-            <Text style={styles.text}>
-              {item.created_at}
-              {item.event_organiser}
-            </Text>
-          </Pressable>
-        )}
+        renderItem={({ item }) => <GroupChatCard item={item} navigation={navigation} />}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-  },
-  event: {
-    padding: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    borderWidth: 2,
-    borderRadius: 14,
-    backgroundColor: "beige",
-    width: 300,
-  },
-  text: {
-    flex: 1,
-    justifyContent: "space-between",
-    textAlign: "center",
-  },
-});

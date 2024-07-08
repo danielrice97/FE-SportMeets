0694a38761c8b9ad5a18dcd0fb30acb020a768a5
AGreaves99 <ajohngreaves@outlook.com>
2024-07-08 11:31:23 +0100

    updates API calls to include getting events a user has joined and stored messages for a group chat

--- sport-meets/api.js
+++ sport-meets/api.js
@@ -1,11 +1,34 @@
-import axios from 'axios'
+import axios from "axios";
 
 const baseApi = axios.create({
-    baseURL: "http://192.168.200.17:9092/api/sportmeets"
-})
+  baseURL: "https://be-sportmeets-py.onrender.com/api/sportmeets"
+});
 
 export function getAllEvents() {
-    return baseApi.get("/events").then((data)=>{
-        return data
+  return baseApi.get("/events").then((data) => {
+    return data;
+  });
+}
+
+export function getUserEvents(username) {
+  return baseApi.get(`/user-events/${username}`).then(({data}) => {
+    return data.UserEvents;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+export function getEventMessages(event_id) {
+  return baseApi.get(`/messages/${event_id}`).then(({data}) => {
+    const {messages} = data
+    return messages.map((message) => {
+      return {...message,
+        created_at: new Date(message.created_at).toLocaleString()
+      }
     })
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 }

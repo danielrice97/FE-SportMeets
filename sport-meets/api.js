71c11ed9a2f4f5eb90b968a700b90aeac403cfac
Alex Straton <alexandrastraton1@gmail.com>
2024-07-09 19:37:42 +0100

    updated api calls with the getAllCategories for the dropdown menu

--- sport-meets/api.js
+++ sport-meets/api.js
@@ -17,106 +17,136 @@ export function getAllEvents(queries) {
   }
 }
 
-
 export function getUser(username) {
-  return baseApi.get(`/users/${username}`).then(({data}) => {
-    return data["user"][0]
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .get(`/users/${username}`)
+    .then(({ data }) => {
+      return data["user"][0];
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function getEventByOrganiser(username) {
-  return baseApi.get(`/events`, {
-    params: {
-      organiser: username
-    }
-  }).then(({data}) => {
-    return data.events
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .get(`/events`, {
+      params: {
+        organiser: username,
+      },
+    })
+    .then(({ data }) => {
+      return data.events;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function postUser(newUser) {
-  return baseApi.post("/users", newUser).then(({data})=> {
-    return data
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .post("/users", newUser)
+    .then(({ data }) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function postEvent(newEvent) {
-  return baseApi.post("/events", newEvent).then((data) => {
-    return data
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .post("/events", newEvent)
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function updateSpacesAvailable(event) {
-  const eventID = event.event_id
-  const eventBody = {...event, 
-    event_spaces_available: event.event_spaces_available - 1
-  }
-  delete eventBody.event_id
-  return baseApi.patch(`/events/${eventID}`, eventBody).then(({data}) => {
-    return data.UpdatedEvent
-  })
+  const eventID = event.event_id;
+  const eventBody = {
+    ...event,
+    event_spaces_available: event.event_spaces_available - 1,
+  };
+  delete eventBody.event_id;
+  return baseApi.patch(`/events/${eventID}`, eventBody).then(({ data }) => {
+    return data.UpdatedEvent;
+  });
 }
 
 export function deleteEvent(event_id) {
-  return baseApi.delete(`/events/${event_id}`).then((data) => {
-    return data
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .delete(`/events/${event_id}`)
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function joinEvent(userevent) {
-  return baseApi.post(`/join-event`, userevent).then((data) => {
-    return data
-  }).catch((err)=> {
-    return err
-  })
+  return baseApi
+    .post(`/join-event`, userevent)
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function getUserEvents() {
-  return baseApi.get("/userevents").then(({data}) => {
-    return data["UserEvents"]
-  })
+  return baseApi.get("/userevents").then(({ data }) => {
+    return data["UserEvents"];
+  });
 }
 
 export function getUserEventsByID(event_id) {
-  return baseApi.get(`/userevents/${event_id}`).then(({data}) => {
-    return data["UserEvents"]
-  })
+  return baseApi.get(`/userevents/${event_id}`).then(({ data }) => {
+    return data["UserEvents"];
+  });
 }
 
 export function getAllUsers() {
-  return baseApi.get("/users").then(({data})=> {
-    return data["users"]
-  })
+  return baseApi.get("/users").then(({ data }) => {
+    return data["users"];
+  });
 }
 
 export function getEventsByUsername(username) {
-  return baseApi.get(`/user-events/${username}`).then(({data})=> {
-    return data["UserEvents"]
-  }).catch((err) => {
-    console.log(err);
-  })
+  return baseApi
+    .get(`/user-events/${username}`)
+    .then(({ data }) => {
+      return data["UserEvents"];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 export function getEventMessages(event_id) {
-  return baseApi.get(`/messages/${event_id}`).then(({data}) => {
-    const {messages} = data
-    return messages.map((message) => {
-      return {...message,
-        created_at: new Date(message.created_at).toLocaleString()
-      }
+  return baseApi
+    .get(`/messages/${event_id}`)
+    .then(({ data }) => {
+      const { messages } = data;
+      return messages.map((message) => {
+        return {
+          ...message,
+          created_at: new Date(message.created_at).toLocaleString(),
+        };
+      });
     })
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export function getAllCategories() {
+  return baseApi.get(`/categories`).then(({ data }) => {
+    return data.Event_Categories;
+  });
 }

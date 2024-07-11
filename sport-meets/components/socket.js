e4a06ad88c25fd682302d49e7df81ae5d1bb7de5
AGreaves99 <ajohngreaves@outlook.com>
2024-07-11 10:25:25 +0100

    updates socketIO url to match hosted address

--- sport-meets/components/socket.js
+++ sport-meets/components/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000"); //   currently needs to be hosted locally
+export const socket = io("https://be-socket-io.onrender.com"); //   currently needs to be hosted locally
